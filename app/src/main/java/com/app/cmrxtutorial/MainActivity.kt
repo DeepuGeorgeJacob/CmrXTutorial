b89ea83cdde3e13446688bf09781b2f606aaf3cd
Deepu George Jacob <deepugeorgejacob@Deepus-MacBook-Pro.local>
2023-12-09 19:11:22 +0100

    Mastering CameraX in Jetpack Compose: A Comprehensive Guide for Android Developers

---
+++ app/src/main/java/com/app/cmrxtutorial/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.app.cmrxtutorial
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.app.cmrxtutorial.composables.CameraPreviewScreen
+import com.app.cmrxtutorial.ui.theme.CmrXTutorialTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val cameraPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                setCameraPreview()
+            } else {
+                // Camera permission denied
+            }
+
+        }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+                setCameraPreview()
+            }
+            else -> {
+                cameraPermissionRequest.launch(Manifest.permission.CAMERA)
+            }
+        }
+
+    }
+    private fun setCameraPreview() {
+        setContent {
+            CmrXTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CameraPreviewScreen()
+                }
+            }
+        }
+    }
+}
